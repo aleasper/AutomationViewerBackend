350f6836d6fd463b83829624b848a4663ce14178
aleasper <alexidest@gmail.com>
2020-12-02 23:09:08 +0300

    fix urls patterns

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/test', views.TestView.as_view())
+    path('test', views.TestView.as_view()),
+    path('vk_data', views.VKDataView.as_view())
 ]
