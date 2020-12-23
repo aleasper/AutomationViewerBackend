f1e5ecfedab640b3e1b1f98a038e362f52e5a93f
aleasper <alexidest@gmail.com>
2020-12-23 22:57:54 +0300

    Add base64 xlsx

--- main_app/views.py
+++ main_app/views.py
@@ -15,6 +15,48 @@ from django.utils.decorators import method_decorator
 
 import json
 
+from io import BytesIO
+from openpyxl import Workbook
+import base64
+
+def get_base64_by_res(res):
+    wb = Workbook()
+    sheet = wb.active
+
+    sheet['A1'] = 'Источник'
+    for k, public in enumerate(res, start=2):
+        sheet[f'A{k}'] = public['info']['name']
+
+    sheet['B1'] = 'Лайки'
+    for k, public in enumerate(res, start=2):
+        sheet[f'B{k}'] = public['info']['likes']
+
+    sheet['C1'] = 'Комменатрии'
+    for k, public in enumerate(res, start=2):
+        sheet[f'C{k}'] = public['info']['comments']
+
+    sheet['D1'] = 'Репосты'
+    for k, public in enumerate(res, start=2):
+        sheet[f'D{k}'] = public['info']['reposts']
+
+    for col in sheet.columns:
+        max_length = 0
+        column = col[0].column_letter  # Get the column name
+        # Since Openpyxl 2.6, the column name is  ".column_letter" as .column became the column number (1-based)
+        for cell in col:
+            try:  # Necessary to avoid error on empty cells
+                if len(str(cell.value)) > max_length:
+                    max_length = len(cell.value)
+            except:
+                pass
+        adjusted_width = (max_length + 2) * 1.2
+        sheet.column_dimensions[column].width = adjusted_width
+
+    virtual_workbook = BytesIO()
+    wb.save(virtual_workbook)
+
+    base64Str = base64.b64encode(virtual_workbook.getvalue())
+    return base64Str
 
 @method_decorator(csrf_exempt, name='dispatch')
 class TestView(View):
@@ -77,6 +119,43 @@ class VKDataView(View):
         db = VKRemoteMDB(app_id)
         res = db.get_all()
         return JsonResponse({'app_info': res})
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class VKDataViewXLSX(View):
+    def get(self, request):
+        try:
+            app_id = request.GET.get('app_id', None)
+            login = request.GET.get('login', None)
+            password = request.GET.get('password', None)
+
+            if login is not None and password is not None:
+                db = VKRemoteMDB('Users')
+                user = db.find({'login': login})
+                print(f'\n\n\n{user}')
+                if user != []:
+                    user_filed = user[0]
+                    if user_filed['password'] == password:
+                        if app_id is not None:
+                            db = VKRemoteMDB(app_id)
+                            res = db.get_all()
+                            base64 = get_base64_by_res(res)
+
+                            return JsonResponse({'base64': base64.decode(), 'ok': True})
+                        else:
+                            return JsonResponse({'error': {'msg': 'no \'app_id\' parameter', 'code': 1}, 'ok': False}, status=400)
+                    else:
+                        return JsonResponse({'error': {'msg': 'wrong login/password', 'code': 4}, 'ok': False},
+                                            status=400)
+                else:
+                    return JsonResponse({'error': {'msg': 'wrong login/password', 'code': 4}, 'ok': False}, status=400)
+            else:
+                return JsonResponse({'error': {'msg': 'wrong login/password', 'code': 4}, 'ok': False}, status=400)
+
+
+        except Exception as e:
+            return JsonResponse({'error': {'msg': f'{e}', 'code': 0}}, status=400)
+
 
 @method_decorator(csrf_exempt, name='dispatch')
 class Registration(View):
---
+++ test_work.py
@@ -0,0 +1,257 @@
+res = {
+    "app_info": [
+        {
+            "_id": -158452705,
+            "info": {
+                "likes": 1,
+                "comments": 1,
+                "reposts": 0,
+                "name": "Пацаны с Авеню́ Монте́нь"
+            }
+        },
+        {
+            "_id": -76040632,
+            "info": {
+                "likes": 123,
+                "comments": 0,
+                "reposts": 3,
+                "name": "хех"
+            }
+        },
+        {
+            "_id": -53845179,
+            "info": {
+                "likes": 84,
+                "comments": 0,
+                "reposts": 1,
+                "name": "че"
+            }
+        },
+        {
+            "_id": -145105562,
+            "info": {
+                "likes": 1,
+                "comments": 2,
+                "reposts": 0,
+                "name": "Мюсли Чпокера"
+            }
+        },
+        {
+            "_id": -77127883,
+            "info": {
+                "likes": 0,
+                "comments": 0,
+                "reposts": 0,
+                "name": "ayy memes"
+            }
+        },
+        {
+            "_id": -178888355,
+            "info": {
+                "likes": 4,
+                "comments": 0,
+                "reposts": 0,
+                "name": "чат с водителем"
+            }
+        },
+        {
+            "_id": -137762094,
+            "info": {
+                "likes": 2,
+                "comments": 0,
+                "reposts": 0,
+                "name": "Геймдев, который мы заслужили"
+            }
+        },
+        {
+            "_id": -29715608,
+            "info": {
+                "likes": 2,
+                "comments": 0,
+                "reposts": 0,
+                "name": "UselessMouth"
+            }
+        },
+        {
+            "_id": -151861811,
+            "info": {
+                "likes": 3,
+                "comments": 0,
+                "reposts": 0,
+                "name": "Патруль"
+            }
+        },
+        {
+            "_id": -110286745,
+            "info": {
+                "likes": 8,
+                "comments": 1,
+                "reposts": 0,
+                "name": "G O S L Y A"
+            }
+        },
+        {
+            "_id": -144592,
+            "info": {
+                "likes": 3,
+                "comments": 0,
+                "reposts": 0,
+                "name": "эстетика отвратительного"
+            }
+        },
+        {
+            "_id": -111587102,
+            "info": {
+                "likes": 4,
+                "comments": 0,
+                "reposts": 0,
+                "name": "киберпанк, который мы заслужили"
+            }
+        },
+        {
+            "_id": -129368275,
+            "info": {
+                "likes": 6,
+                "comments": 4,
+                "reposts": 1,
+                "name": "отчислено"
+            }
+        },
+        {
+            "_id": -142203134,
+            "info": {
+                "likes": 8,
+                "comments": 0,
+                "reposts": 0,
+                "name": "крайне некультурные папуги"
+            }
+        },
+        {
+            "_id": -126771813,
+            "info": {
+                "likes": 6,
+                "comments": 3,
+                "reposts": 0,
+                "name": "Сасамба"
+            }
+        },
+        {
+            "_id": -97902875,
+            "info": {
+                "likes": 18,
+                "comments": 0,
+                "reposts": 0,
+                "name": "еда не еда"
+            }
+        },
+        {
+            "_id": -73011687,
+            "info": {
+                "likes": 5,
+                "comments": 0,
+                "reposts": 0,
+                "name": "ниче"
+            }
+        },
+        {
+            "_id": -101621324,
+            "info": {
+                "likes": 3,
+                "comments": 0,
+                "reposts": 0,
+                "name": "Толстые собаки и цитаты о любви"
+            }
+        },
+        {
+            "_id": -156566409,
+            "info": {
+                "likes": 7,
+                "comments": 1,
+                "reposts": 0,
+                "name": "RIWKA"
+            }
+        },
+        {
+            "_id": -92337511,
+            "info": {
+                "likes": 15,
+                "comments": 0,
+                "reposts": 0,
+                "name": "абстрактные мемы для элиты всех сортов | АМДЭВС"
+            }
+        },
+        {
+            "_id": -158619277,
+            "info": {
+                "likes": 8,
+                "comments": 0,
+                "reposts": 0,
+                "name": "мемы для людей с отклонениями"
+            }
+        },
+        {
+            "_id": -126988526,
+            "info": {
+                "likes": 2,
+                "comments": 0,
+                "reposts": 0,
+                "name": "вsратые животные"
+            }
+        },
+        {
+            "_id": -23213239,
+            "info": {
+                "likes": 33,
+                "comments": 0,
+                "reposts": 0,
+                "name": "Дзен"
+            }
+        },
+        {
+            "_id": -3653319,
+            "info": {
+                "likes": 4,
+                "comments": 0,
+                "reposts": 0,
+                "name": "ОКО"
+            }
+        },
+        {
+            "_id": -68995594,
+            "info": {
+                "likes": 27,
+                "comments": 0,
+                "reposts": 0,
+                "name": "HistoryPorn"
+            }
+        },
+        {
+            "_id": -158275105,
+            "info": {
+                "likes": 3,
+                "comments": 0,
+                "reposts": 0,
+                "name": "Лига плохих шуток"
+            }
+        },
+        {
+            "_id": -148559209,
+            "info": {
+                "likes": 27,
+                "comments": 0,
+                "reposts": 0,
+                "name": "Гулька"
+            }
+        }
+    ],
+    "ok": True
+}
+
+
+
+'''base64.b64encode(bytes.getvalue())'''
+
+
+if __name__ == '__main__':
+    #print(get_base64_by_res(res))
+    pass
